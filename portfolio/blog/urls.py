57fab8df306a05d3a016179247340fa8f07acc17
unknown <saurab.shrestha99@gmail.com>
2019-12-27 17:23:26 +0545

    after add delete update blog post

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -1,8 +1,19 @@
 from . import views
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,PostDeleteView
 
 urlpatterns = [
-    path('', views.allblogs, name='allblogs'),
-    path('<int:blog_id>', views.detail, name='detailblog'),
+    # path('', views.allblogs, name='allblogs'),
+    path('', PostListView.as_view(), name='allblogs'),
+
+    # path('<int:blog_id>', views.detail, name='detailblog'),
+
+    path('<int:pk>/', PostDetailView.as_view(), name='detailblog'),
+
+    path('new/', PostCreateView.as_view(), name='post-create'),
+
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,15 +1,58 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Blog
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.urls import reverse_lazy
 
 # Create your views here.
-def allblogs(request):
-    blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+# def allblogs(request):
+#     blogs = Blog.objects
+#     return render(request, 'blog/allblogs.html', {'blogs':blogs})
+
+class PostListView(ListView):
+    model = Blog
+    template_name = 'blog/allblogs.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'blogs'
 
 
-def detail(request, blog_id):
-    detailBlog = get_object_or_404(Blog, pk = blog_id)
-    return render(request, 'blog/detail.html', {'blog':detailBlog})
+
+
+#
+# def detail(request, blog_id):
+#     detailBlog = get_object_or_404(Blog, pk = blog_id)
+#     return render(request, 'blog/blog_detail.html', {'blog':detailBlog})
+
+class PostDetailView(DetailView):
+    model = Blog
+
+class PostCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'body','image']
+
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super().form_valid(form)
+
+    success_url =reverse_lazy('allblogs')
+
+class PostUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'body','image']
+    template_name = 'blog/blog_update.html'
+    success_url =reverse_lazy('allblogs')
+
+class PostDeleteView(DeleteView):
+    model = Blog
+    # success_url = 'allblogs'
+    success_url =reverse_lazy('allblogs')
+
+
+
 
 
 
